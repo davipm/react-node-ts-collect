4a32e623145f734a963afc77aa1749d4f9c3dd52
davi <davi.p.m94@gmail.com>
2020-07-16 13:17:12 -0300

    create Dropzone component

--- client/src/components/Dropzone/index.tsx
+++ client/src/components/Dropzone/index.tsx
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+import { FiUpload } from "react-icons/fi";
 
-function Dropzone() {
+import { Container } from "./styles";
+
+interface Props {
+  onFileUpload: (file: File) => void;
+}
+
+function Dropzone({ onFileUpload }: Props) {
+  const [selectedFileUrl, setSelectedFileUrl] = useState("");
+
+  const onDrop = useCallback(
+    (acceptedFiles) => {
+      const file = acceptedFiles[0];
+      const fileUrl = URL.createObjectURL(file);
+
+      setSelectedFileUrl(fileUrl);
+      onFileUpload(file);
+    },
+    [onFileUpload]
+  );
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: "image/*",
+  });
+
   return (
-    <div>
-      <p>Drop</p>
-    </div>
+    <Container {...getRootProps()}>
+      <input {...getInputProps()} />
+      {selectedFileUrl ? (
+        <img src={selectedFileUrl} alt="Point Thumbnail" />
+      ) : (
+        <span>
+          <FiUpload />
+          Property image
+        </span>
+      )}
+    </Container>
   );
 }
 
--- client/src/components/Dropzone/styles.ts
+++ client/src/components/Dropzone/styles.ts
@@ -1,3 +1,38 @@
-import styled from "styled-components";
+import styled from "styled-components/macro";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 48px;
+  outline: 0;
+  height: 300px;
+  background-color: var(--dopzone-bg-color);
+  border-radius: 10px;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  span {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: calc(100% - 60px);
+    height: calc(100% - 60px);
+    color: #333;
+    border: 1px dashed var(--dopzone-border-color);
+    border-radius: 10px;
+    cursor: pointer;
+
+    svg {
+      color: var(--dopzone-border-color);
+      width: 24px;
+      height: 24px;
+      margin-bottom: 8px;
+    }
+  }
+`;
